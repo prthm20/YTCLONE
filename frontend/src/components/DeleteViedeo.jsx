f6f6d765c59beed4629819ae03d1f0b3b0b63771
prthm20 <aprathamm@gmail.com>
2024-06-18 16:25:19 +0530

    Y

--- frontend/src/components/DeleteViedeo.jsx
+++ frontend/src/components/DeleteViedeo.jsx
@@ -5,12 +5,12 @@ import axios from 'axios';
 const DeleteVideo = ({ videoId }) => {
     const handleDelete = async () => {
         try {
-            await axios.delete(`https://va-backend-mu.vercel.app/api/v1/users/videos/${videoId}`);
+            await axios.delete(`https://va-backend-mu.vercel.app/api/v1/users/viedeos/${videoId}`);
             console.log('Video deleted successfully');
             // Optionally, update the UI or perform any other action after deletion
         } catch (error) {
             console.error('Error deleting video:', error);
-            // Handle error, e.g., show an error message
+          
         }
     };
 
